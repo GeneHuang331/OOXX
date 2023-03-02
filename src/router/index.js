e3a4902090bd9fcc61d3ad1cb0d835d48340f3b8
Gene <tutor8110221510@gmail.com>
2023-03-02 17:04:47 +0800

    增加 vue router設定，view 組件分離

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import App from '@/App.vue'
+import login from '@/views/login.vue'
+import gameView from '@/views/gameView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/:id',
+      redirect: (to) => {
+        // the function receives the target route as the argument
+        // a relative location doesn't start with `/`
+        // or { path: 'profile'}
+        return '/'
+      }
+    },
+    {
       path: '/',
-      name: 'home',
-      component: App
+      name: 'login',
+      component: login
+    },
+    {
+      path: '/start',
+      name: 'gameView',
+      component: gameView
     }
   ]
 })
---
+++ src/views/gameView.vue
@@ -0,0 +1,51 @@
+<template>
+  <main class="main">
+    <header>
+      <div>O:{{ score.O }},X:{{ score.X }}</div>
+    </header>
+    <checkerBoard
+      :key="checkerboardKey"
+      @addCheckerboardKey="addCheckerboardKey()"
+      @addScore="addScore"
+    ></checkerBoard>
+  </main>
+</template>
+<script>
+import checkerBoard from '@/components/checkerBoard.vue'
+export default {
+  components: { checkerBoard },
+  data() {
+    return {
+      checkerboardKey: 0,
+      winScore: 3,
+      score: {
+        O: 0,
+        X: 0
+      }
+    }
+  },
+  methods: {
+    addCheckerboardKey() {
+      this.checkerboardKey++
+    },
+    addScore(winner) {
+      this.score[winner]++
+    }
+  },
+  created() {
+    // console.log(this.judge(this.ooxxArr))
+  }
+}
+</script>
+<style scoped lang="scss">
+.main {
+  max-width: 900px;
+  height: 100vh;
+  width: 100vw;
+  background: rgb(146, 146, 146);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: flex-start;
+}
+</style>
