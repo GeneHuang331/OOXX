5c845a7c4d93ebd1fdd10c04bdb2921028ecb31a
Gene <tutor8110221510@gmail.com>
2023-03-02 17:28:33 +0800

    禁止雙擊放大

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,1 +1,34 @@
-<template>login</template>
+<template>
+  <main>
+    <button class="login" @click="login()">登入</button>
+  </main>
+</template>
+<script>
+export default {
+  methods: {
+    login() {
+      console.log(this.$router)
+      this.$router.push({ path: '/start' })
+    }
+  }
+}
+</script>
+<style lang="scss">
+main {
+  width: 100%;
+  max-width: 900px;
+  margin: 0 auto;
+  height: 100vh;
+  background: red;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  .login {
+    background: green;
+    width: 150px;
+    height: 50px;
+    font-size: 1rem;
+    color: #fff;
+  }
+}
+</style>
