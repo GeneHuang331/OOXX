8624cf5db55c14db8d4d5073c3db2b6592e920d3
Gene <tutor8110221510@gmail.com>
2023-03-02 11:50:01 +0800

    修改vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 
 export default defineConfig({
-  publicPath: '',
+  publicPath: '/OOXX/',
   plugins: [vue()],
   resolve: {
     alias: {
